15c4b8b5e2f236c50c52cb7e8963117a22aea48c
koresachin7 <koresachin7@gmail.com>
2021-10-28 23:38:20 +0530

    Added:-  To load state code from CSV

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,7 +1,32 @@
 package com.bridgelabz;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Purpose : To implement Indian State Census Analyser
+ *
+ * @author Sachin Kore
+ * @version 16.0
+ */
 public class CensusAnalyser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, StateSensorAnalyserException {
         System.out.println("Welcome to Indian States Census Analyser");
+        String filePathRead = ".src/main/resources/IndianStatesCensusAnalyser.csv";
+        String fileName = "IndianStateCensusData";
+        String delimiter =",";
+        List<String> stringName = new ArrayList<>();
+        stringName.add("State");
+        stringName.add("Population");
+        stringName.add("AreaInSqKm");
+        stringName.add("DensityPerSqKm");
+
+        ReadOperations readObj = new ReadOperations();
+        int count = readObj.readDataCount(filePathRead, fileName);
+        System.out.println(count);
+
+        readObj.readDelimiter(filePathRead, delimiter);
+        readObj.readHeader(filePathRead, stringName);
     }
 }
